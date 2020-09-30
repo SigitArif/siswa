8715eb7de633cee61ee5c1afe06a421002ce5e8d
Sigit Arif Anggoro <sigit.arifanggoro@ai.astra.co.id>
2020-09-30 22:05:06 +0700

    modify many to one relationship

--- src/main/java/com/example/siswa/entity/Nilai.java
+++ src/main/java/com/example/siswa/entity/Nilai.java
@@ -2,8 +2,11 @@ package com.example.siswa.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 
@@ -15,20 +18,23 @@ public class Nilai {
     @Column(name = "ID_NILAI")
     private Long id;
 
-    @Column(name ="NOMOR_INDUK_SISWA")
-    private Long idSiswa;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="NOMOR_INDUK_SISWA")
+    private Siswa siswa;
 
-    @Column(name = "ID_MAPEL")
-    private Long idMapel;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ID_MAPEL")
+    private Mapel mapel;
 
     private Integer nilai;
 
     public Nilai(){}
 
-    public Nilai(Long id, Long idSiswa, Long idMapel, Integer nilai){
+    public Nilai(Long id, Siswa siswa, Mapel mapel, Integer nilai){
         this.id = id;
-        this.idSiswa = idSiswa;
-        this.idMapel = idMapel;
+        this.siswa = siswa;
+        this.mapel = mapel;
         this.nilai = nilai;
     }
 
